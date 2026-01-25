9a5154e523344e1c296cd1dee2e134ed0d3ccc8d
Cannon Spencer <cannon.cms@gmail.com>
2026-01-25 06:37:05 -0500

    ports

--- include/config/blue/ports.hpp
+++ include/config/blue/ports.hpp
@@ -5,32 +5,32 @@
 namespace robot {
 namespace ports {
 // Drive motors (8 total)
-inline constexpr std::int8_t DRIVE_RIGHT_FRONT      = 0;
-inline constexpr std::int8_t DRIVE_RIGHT_MIDDLE     = 0;
-inline constexpr std::int8_t DRIVE_RIGHT_REAR       = 0;
-inline constexpr std::int8_t DRIVE_RIGHT_AUX        = 0;
+inline constexpr std::int8_t DRIVE_RIGHT_FRONT      = 7;
+inline constexpr std::int8_t DRIVE_RIGHT_MIDDLE     = -8;
+inline constexpr std::int8_t DRIVE_RIGHT_REAR       = 9;
+inline constexpr std::int8_t DRIVE_RIGHT_AUX        = 20;
 
-inline constexpr std::int8_t DRIVE_LEFT_FRONT       = 0;
-inline constexpr std::int8_t DRIVE_LEFT_MIDDLE      = 0;
-inline constexpr std::int8_t DRIVE_LEFT_REAR        = 0;
-inline constexpr std::int8_t DRIVE_LEFT_AUX         = 0;
+inline constexpr std::int8_t DRIVE_LEFT_FRONT       = -2;
+inline constexpr std::int8_t DRIVE_LEFT_MIDDLE      = 5;
+inline constexpr std::int8_t DRIVE_LEFT_REAR        = -3;
+inline constexpr std::int8_t DRIVE_LEFT_AUX         = -11;
 
 // Intake motors (4 total)
-inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_1  = 0;
-inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_2  = 0;
-inline constexpr std::int8_t INTAKE_TOP_STAGE       = 0;
-inline constexpr std::int8_t INTAKE_INDEXER         = 0;
+inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_1  = 1;
+inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_2  = -10;
+inline constexpr std::int8_t INTAKE_TOP_STAGE       = 19;
+inline constexpr std::int8_t INTAKE_INDEXER         = 12;
 
 // Pneumatics
-inline constexpr std::int8_t PISTON_MATCHLOADER     = 0;
-inline constexpr std::int8_t PISTON_INDEXER         = 0;
-inline constexpr std::int8_t PISTON_WING            = 0;
-inline constexpr std::int8_t PISTON_HOOD            = 0;
+inline constexpr std::int8_t PISTON_MATCHLOADER     = 'A';
+inline constexpr std::int8_t PISTON_INDEXER         = 'B';
+inline constexpr std::int8_t PISTON_WING            = 'C';
+inline constexpr std::int8_t PISTON_HOOD            = 'D';
 
 // Sensors
-inline constexpr std::int8_t IMU                    = 0;
+inline constexpr std::int8_t IMU                    = 18;
 inline constexpr std::int8_t VERTICAL_ROTATION      = 0;
 inline constexpr std::int8_t HORIZONTAL_ROTATION    = 0;
-inline constexpr std::int8_t OPTICAL_COLOR_SORT     = 0;
+inline constexpr std::int8_t OPTICAL_COLOR_SORT     = 14;
 }  // namespace ports
 }  // namespace robot
