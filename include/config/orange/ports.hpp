7972c0061c4473edbb7dc2eebd71867409bbb990
ratsareweird <alecninja5000@gmail.com>
2026-01-31 03:23:59 -0600

    Orange ports

--- include/config/orange/ports.hpp
+++ include/config/orange/ports.hpp
@@ -5,27 +5,27 @@
 namespace robot {
 namespace ports {
 // Drive motors (8 total)
-inline constexpr std::int8_t DRIVE_RIGHT_FRONT      = 0;
-inline constexpr std::int8_t DRIVE_RIGHT_MIDDLE     = 0;
-inline constexpr std::int8_t DRIVE_RIGHT_REAR       = 0;
-inline constexpr std::int8_t DRIVE_RIGHT_AUX        = 0;
+inline constexpr std::int8_t DRIVE_RIGHT_FRONT      = 17;
+inline constexpr std::int8_t DRIVE_RIGHT_MIDDLE     = -18;
+inline constexpr std::int8_t DRIVE_RIGHT_REAR       = 19;
+inline constexpr std::int8_t DRIVE_RIGHT_AUX        = 20;
 
-inline constexpr std::int8_t DRIVE_LEFT_FRONT       = 0;
-inline constexpr std::int8_t DRIVE_LEFT_MIDDLE      = 0;
-inline constexpr std::int8_t DRIVE_LEFT_REAR        = 0;
-inline constexpr std::int8_t DRIVE_LEFT_AUX         = 0;
+inline constexpr std::int8_t DRIVE_LEFT_FRONT       = -13;
+inline constexpr std::int8_t DRIVE_LEFT_MIDDLE      = 14;
+inline constexpr std::int8_t DRIVE_LEFT_REAR        = -12;
+inline constexpr std::int8_t DRIVE_LEFT_AUX         = -11;
 
 // Intake motors (4 total)
-inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_1  = 0;
-inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_2  = 0;
-inline constexpr std::int8_t INTAKE_TOP_STAGE       = 0;
-inline constexpr std::int8_t INTAKE_INDEXER         = 0;
+inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_1  = -10;
+inline constexpr std::int8_t INTAKE_BOTTOM_STAGE_2  = 1;
+inline constexpr std::int8_t INTAKE_TOP_STAGE       = 9;
+inline constexpr std::int8_t INTAKE_INDEXER         = 2;
 
 // Pneumatics
-inline constexpr std::int8_t PISTON_MATCHLOADER     = 0;
-inline constexpr std::int8_t PISTON_INDEXER         = 0;
-inline constexpr std::int8_t PISTON_WING            = 0;
-inline constexpr std::int8_t PISTON_HOOD            = 0;
+inline constexpr std::int8_t PISTON_MATCHLOADER     = 'D';
+inline constexpr std::int8_t PISTON_INDEXER         = 'A';
+inline constexpr std::int8_t PISTON_WING            = 'C';
+inline constexpr std::int8_t PISTON_HOOD            = 'B';
 
 // Sensors
 inline constexpr std::int8_t IMU                    = 0;
