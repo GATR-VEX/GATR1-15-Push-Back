87e6ef83dabc6c7d04deb6dc3f2c6df8066f02da
LThmly <logancthomley@gmail.com>
2026-02-14 01:39:42 -0500

    DONT MERGE skills for orange to copy

--- src/config/blue/autons.cpp
+++ src/config/blue/autons.cpp
@@ -219,17 +219,24 @@ void elims_auton(subsystems::color_sort::Color color) {
 
 void skills_auton() {
     // Drive Backwards
-    chassis.pid_drive_set(-47_in, DRIVE_SPEED, true);
+    chassis.pid_drive_set(-31_in, DRIVE_SPEED, true);
     chassis.pid_wait();
 
-    // Turn to face matchloader 
+    // Turn to face long goal 
     chassis.pid_turn_set(-90_deg, TURN_SPEED, true);
     chassis.pid_wait();
+
+    // Drive into long goal and score
+    chassis.pid_drive_set(-25_in, DRIVE_SPEED, true);
+    chassis.pid_wait();
+    subsystems::intake::score_long();
+    pros::delay(500);
+    subsystems::intake::stop();
 
     // Drive into matchloader 1
     subsystems::pistons::safe_extend(subsystems::matchloader);
     subsystems::intake::collect();
-    chassis.pid_drive_set(10_in, DRIVE_SPEED, true);
+    chassis.pid_drive_set(35_in, DRIVE_SPEED, true);
     chassis.pid_wait();
 
     subsystems::color_sort::wait_for_ball(MATCHLOAD_TIMEOUT_MS);
@@ -240,24 +247,24 @@ void skills_auton() {
     chassis.pid_wait();
     subsystems::pistons::safe_retract(subsystems::matchloader);
 
-    chassis.pid_swing_set(ez::RIGHT_SWING, 0_deg, SWING_SPEED, 45); // params: direction, angle, speed, still side speed
-    chassis.pid_wait();
+    // chassis.pid_swing_set(ez::RIGHT_SWING, 0_deg, SWING_SPEED, 45); // params: direction, angle, speed, still side speed
+    // chassis.pid_wait();
 
-    chassis.pid_turn_set(-90_deg, TURN_SPEED, true);
-    chassis.pid_wait();
-    chassis.pid_drive_set(-70_in, DRIVE_SPEED, true);
-    chassis.pid_wait();
+    // chassis.pid_turn_set(-90_deg, TURN_SPEED, true);
+    // chassis.pid_wait();
+    // chassis.pid_drive_set(-70_in, DRIVE_SPEED, true);
+    // chassis.pid_wait();
 
-    // swing to align with long goal
-    chassis.pid_swing_set(ez::LEFT_SWING, 0_deg, SWING_SPEED, 45); // params: direction, angle, speed, still side speed
-    chassis.pid_wait();
+    // // swing to align with long goal
+    // chassis.pid_swing_set(ez::LEFT_SWING, 0_deg, SWING_SPEED, 45); // params: direction, angle, speed, still side speed
+    // chassis.pid_wait();
 
-    // drive into long goal
-    chassis.pid_drive_set(-7_in, DRIVE_SPEED, true);
-    chassis.pid_wait();
-    subsystems::intake::score_long();
-    pros::delay(5000);
-    subsystems::intake::stop();
+    // // drive into long goal
+    // chassis.pid_drive_set(-7_in, DRIVE_SPEED, true);
+    // chassis.pid_wait();
+    // subsystems::intake::score_long();
+    // pros::delay(5000);
+    // subsystems::intake::stop();
 }
 
 
